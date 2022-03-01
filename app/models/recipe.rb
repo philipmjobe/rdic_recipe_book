197c7601eb7a064ca1b0dae3f7eb890405bf315c
Philip <philipmjobe@gmail.com>
2022-03-01 16:18:11 -0500

    updates recipe model to split the ingredients and directions lists

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,11 @@
 class Recipe < ApplicationRecord
   validates :name, presence: true
+
+  def ingredients_list
+    ingredients.split(", ")
+  end
+
+  def directions_list
+    directions.split(". ")
+  end
 end
