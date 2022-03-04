b8798af970d94bc178b2739b4e63b85bff3b2044
Philip <philipmjobe@gmail.com>
2022-03-04 14:38:46 -0500

    adds update action to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -25,4 +25,21 @@ class RecipesController < ApplicationController
       status: 422
     end
   end 
+
+  def update
+    recipe_id = params["id"]
+    recipe = Recipe.find_by(id: recipe_id)
+
+    recipe.name = params["name"] || recipe.name
+    recipe.contributor = params["contributor"] || recipe.contributor
+    recipe.ingredients = params["ingredients"] || recipe.ingredients
+    recipe.directions = params["directions"] || recipe.directions
+
+    if recipe.save
+      render json: recipe
+    else 
+      render json: {errors: recipe.errors.full_messages},
+      status: 422
+    end 
+  end 
 end
