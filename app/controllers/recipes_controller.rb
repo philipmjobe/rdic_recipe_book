d37934c700b845bc18596da9aea40ca1851a9b76
Philip <philipmjobe@gmail.com>
2022-03-04 14:32:51 -0500

    adds create action to recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -9,5 +9,20 @@ class RecipesController < ApplicationController
     recipe = Recipe.find_by(id: params[:id])
     render json: recipe
   end 
-  
+
+  def create
+    recipe = Recipe.new(
+      name: params["name"],
+      contributor: params["contributor"],
+      ingredients: params["ingredients"],
+      directions: params[:directions]
+    )
+
+    if recipe.save
+      render json: recipe
+    else
+      render json: {errors: recipe.errors.full_messages},
+      status: 422
+    end
+  end 
 end
