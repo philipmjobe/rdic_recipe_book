4b8d6eb195502697ebba2aa72a8db5dd64376f0a
Philip <philipmjobe@gmail.com>
2022-05-17 15:25:18 -0400

    adds notes logic and tests to 200 ok in request.http

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,3 +1,3 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :id, :name, :ingredients, :directions, :contributor, :category, :ingredients_list, :directions_list
+  attributes :id, :name, :ingredients, :directions, :contributor, :category, :ingredients_list, :directions_list, :notes
 end
