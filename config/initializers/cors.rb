f3977a2689caf43d84e89bc53ccb45a041c439bb
Philip <philipmjobe@gmail.com>
2022-06-01 15:20:41 -0400

    updates cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:8080'
+    origins 'localhost:8080', 'https://incredible-tarsier-6e797e.netlify.app'
 
     resource '*',
       headers: :any,
