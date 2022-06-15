edc264d3da0c78334d83cbf7430bc75654054c94
Philip <philipmjobe@gmail.com>
2022-06-15 13:43:57 -0400

    adds notes show route to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,6 @@ Rails.application.routes.draw do
   post "/recipes" => "recipes#create"
   patch "/recipes/:id" => "recipes#update"
   delete "/recipes/:id" => "recipes#destroy"
+
+  get "/notes/:id" => "notes#show"
 end
